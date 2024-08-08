71c2139c6643c0c252ddc45320c9c464d6aa7ae1
Ankit Dash <ankitdash3037@gmail.com>
2024-08-09 00:45:21 +0530

    Changes in node modules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const { spawn } = require('child_process');
 
